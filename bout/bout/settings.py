87572163f1bf3d57ba71d13ea83b40b31b344b23
01Laurent <kiirulaurent@gmail.com>
2024-09-25 19:44:17 +0300

    favicon

--- bout/bout/settings.py
+++ bout/bout/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from celery.schedules import crontab
 CELERY_BEAT_SCHEDULE = {
@@ -137,6 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
